529d626f6f420151bb351d59579d74e5bd29a460
Morteza Nedaei <nedaei1375@gmail.com>
2022-05-16 20:39:10 +0430

    Feat(presentation-viewmodel): Update Viewmodel using new data retrieved from different UseCases

--- app/src/main/java/com/mooncascade/presentation/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mooncascade/presentation/ui/home/HomeViewModel.kt
@@ -3,10 +3,13 @@ package com.mooncascade.presentation.ui.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mooncascade.data.di.qualifier.IoDispatcher
-import com.mooncascade.data.entity.ForecastEntity
+import com.mooncascade.data.entity.current.CurrentWeatherEntity
+import com.mooncascade.data.entity.forecast.ForecastEntity
 import com.mooncascade.data.respository.WeatherDataRepository
-import com.mooncascade.domain.interactor.GetForecastsUseCase
+import com.mooncascade.domain.interactor.GetCurrentWeatherUseCase
+import com.mooncascade.domain.interactor.GetNextDaysForecastsUseCase
 import com.mooncascade.domain.model.ViewState
+import com.mooncascade.domain.model.WeatherType
 import com.mooncascade.presentation.utils.Constants
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
@@ -18,7 +21,8 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val repository: WeatherDataRepository,
-    private val useCase: GetForecastsUseCase,
+    private val nextDaysForecastsUseCase: GetNextDaysForecastsUseCase,
+    private val currentWeatherUseCase: GetCurrentWeatherUseCase,
     @IoDispatcher private val coroutineDispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
@@ -26,23 +30,51 @@ class HomeViewModel @Inject constructor(
         private const val TAG = "HomeViewModel"
     }
 
-    private val _forecastsFlow = MutableStateFlow<ViewState<List<ForecastEntity>>>(ViewState.Idle)
-    val forecastsFlow get() = _forecastsFlow
+    private val _currentWeatherFlow =
+        MutableStateFlow<ViewState<CurrentWeatherEntity>>(ViewState.Idle)
+    val currentWeatherFlow get() = _currentWeatherFlow
+
+    private val _nextDaysForecastsFlow =
+        MutableStateFlow<ViewState<List<ForecastEntity>>>(ViewState.Idle)
+    val nextDaysForecastsFlow get() = _nextDaysForecastsFlow
 
     init {
-        getForecasts()
+        getCurrentWeather()
+        getNextDaysForecasts()
     }
 
-    private fun getForecasts() = viewModelScope.launch(coroutineDispatcher) {
-        _forecastsFlow.value = ViewState.Loading
-        useCase.invoke()
+    private fun getCurrentWeather() = viewModelScope.launch(coroutineDispatcher) {
+        _currentWeatherFlow.value = ViewState.Loading
+        currentWeatherUseCase.invoke()
             .catch { e ->
-                _forecastsFlow.value =
+                _currentWeatherFlow.value =
                     ViewState.Error(e.message ?: "$TAG: ${Constants.ERROR_GET_FORECASTS}")
             }
             .collect { data ->
-                _forecastsFlow.value = ViewState.Success(data.getOrNull()?.forecasts ?: emptyList())
+                _currentWeatherFlow.value = ViewState.Success(data.getOrNull())
             }
+    }
+
+    private fun getNextDaysForecasts() = viewModelScope.launch(coroutineDispatcher) {
+        _nextDaysForecastsFlow.value = ViewState.Loading
+        nextDaysForecastsUseCase.invoke()
+            .catch { e ->
+                _nextDaysForecastsFlow.value =
+                    ViewState.Error(e.message ?: "$TAG: ${Constants.ERROR_GET_FORECASTS}")
+            }
+            .collect { data ->
+                _nextDaysForecastsFlow.value =
+                    ViewState.Success(data.getOrNull()?.forecasts ?: emptyList())
+            }
+    }
+
+
+    fun getCurrentWeatherAnimation(phenomenon: String?): Int {
+        val weatherAnim = WeatherType.values().find { weatherType ->
+            weatherType.weatherDescription.contains(phenomenon!!)
+        }?.weatherAnim
+
+        return weatherAnim ?: WeatherType.UNKNOWN.weatherAnim
     }
 
 }

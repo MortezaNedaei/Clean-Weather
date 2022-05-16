da12de4d3a28d19fd240c436e2df5a5daa51ba26
Morteza Nedaei <nedaei1375@gmail.com>
2022-05-16 20:19:14 +0430

    Feat(utils): add new object for locations constants

--- app/src/main/java/com/mooncascade/presentation/utils/Constants.kt
+++ app/src/main/java/com/mooncascade/presentation/utils/Constants.kt
@@ -2,4 +2,8 @@ package com.mooncascade.presentation.utils
 
 object Constants {
     const val ERROR_GET_FORECASTS = "Error in getForecasts"
+
+    object LOCATION {
+        const val CODE_TARTU = "26242"
+    }
 }

c7a1ecfde9d245e96e9cac1b7f2053b5c9fb5df9
Morteza Nedaei <nedaei1375@gmail.com>
2022-05-19 16:31:11 +0430

    Test(coroutine): add test requirement rule for the Coroutines

---
+++ app/src/test/java/com/mooncascade/utils/DispatcherProvider.kt
@@ -0,0 +1,31 @@
+package com.mooncascade.utils
+
+import androidx.lifecycle.*
+import kotlinx.coroutines.CoroutineDispatcher
+
+interface DispatcherProvider {
+    val io: CoroutineDispatcher
+    val ui: CoroutineDispatcher
+    val default: CoroutineDispatcher
+    val unconfined: CoroutineDispatcher
+}
+
+fun <T> LiveData<T>.observeOnce(onChangeHandler: (T) -> Unit) {
+    val observer = OneTimeObserver(handler = onChangeHandler)
+    observe(observer, observer)
+}
+
+class OneTimeObserver<T>(private val handler: (T) -> Unit) : Observer<T>, LifecycleOwner {
+    private val lifecycle = LifecycleRegistry(this)
+
+    init {
+        lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    }
+
+    override fun getLifecycle(): Lifecycle = lifecycle
+
+    override fun onChanged(t: T) {
+        handler(t)
+        lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    }
+}

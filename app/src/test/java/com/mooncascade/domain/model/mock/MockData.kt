be41c594c2c1e1bc4e8b824e7c2bb2b149ca9e1f
Morteza Nedaei <nedaei1375@gmail.com>
2022-05-21 01:59:10 +0430

    Feat(test): add a mock file used to hold some fake data for testing

---
+++ app/src/test/java/com/mooncascade/domain/model/mock/MockData.kt
@@ -0,0 +1,8 @@
+package com.mooncascade.domain.model.mock
+
+public val currentWeatherMockData = "{\n" +
+        "  \"timestamp\": \"1652689045\"}"
+
+
+public val locationWeatherMockData = "{\n" +
+        "  \"code\": \"26038\"}"

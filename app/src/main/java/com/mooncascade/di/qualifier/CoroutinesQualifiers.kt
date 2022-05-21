6874f052a8d45fd9a9394036216d0c12846a4cd7
Morteza Nedaei <nedaei1375@gmail.com>
2022-05-21 17:58:21 +0430

    Fix(package): Fix package name

--- app/src/main/java/com/mooncascade/di/qualifier/CoroutinesQualifiers.kt
+++ app/src/main/java/com/mooncascade/di/qualifier/CoroutinesQualifiers.kt
@@ -6,7 +6,7 @@ import javax.inject.Qualifier
 /**
  * This class uses qualifier which is an annotation that you use to identify a specific
  * binding for a type when that type has multiple bindings defined.
- * @see [com.mooncascade.data.di.CoroutinesModule]
+ * @see [com.mooncascade.di.CoroutinesModule]
  *
  */
 

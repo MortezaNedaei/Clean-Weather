899ab8bfbcb62ec24b558649f4dc9fb760e6c937
Morteza Nedaei <nedaei1375@gmail.com>
2022-05-22 21:45:23 +0430

    Feat(data): Add db configs

--- app/src/main/java/com/mooncascade/data/entity/forecast/PlaceEntity.kt
+++ app/src/main/java/com/mooncascade/data/entity/forecast/PlaceEntity.kt
@@ -1,11 +1,22 @@
 package com.mooncascade.data.entity.forecast
 
 
+import android.os.Parcelable
 import androidx.annotation.Keep
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
 @Keep
+@Parcelize
+@Entity(
+    tableName = "Place",
+    indices = [Index(value = ["id"], unique = true)]
+)
 data class PlaceEntity(
+    @PrimaryKey val id: String,
     @SerializedName("name")
     val name: String?,
     @SerializedName("phenomenon")
@@ -14,4 +25,4 @@ data class PlaceEntity(
     val tempmax: Int?,
     @SerializedName("tempmin")
     val tempmin: Int?
-)
+) : Parcelable

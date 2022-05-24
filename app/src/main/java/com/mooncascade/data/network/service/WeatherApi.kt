55c17026a01255a501e118dc50adda921dd62990
Morteza Nedaei <nedaei1375@gmail.com>
2022-05-24 18:56:03 +0430

    Refactor(data): repackaged, Update WebServices data type

--- app/src/main/java/com/mooncascade/data/network/service/WeatherApi.kt
+++ app/src/main/java/com/mooncascade/data/network/service/WeatherApi.kt
@@ -1,9 +1,9 @@
-package com.mooncascade.data.network
+package com.mooncascade.data.network.service
 
-import com.mooncascade.data.entity.current.CurrentWeatherEntity
-import com.mooncascade.data.entity.forecast.NextDaysForecastEntity
 import com.mooncascade.data.entity.forecast.ForecastEntity
-import com.mooncascade.data.entity.location.LocationEntity
+import com.mooncascade.data.network.entity.current.CurrentWeatherResponse
+import com.mooncascade.data.network.entity.forecast.NextDaysForecastResponse
+import com.mooncascade.data.network.entity.location.LocationResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -25,22 +25,22 @@ interface WeatherApi {
      * @return forecasts: [ForecastEntity]
      */
     @GET(Endpoints.GET_FORECASTS)
-    suspend fun getNextDaysForecasts(): Response<NextDaysForecastEntity>
+    suspend fun getNextDaysForecasts(): Response<NextDaysForecastResponse>
 
     /**
      * retrieves current weather status
-     * @return current weather: [CurrentWeatherEntity]
+     * @return current weather: [CurrentWeatherResponse]
      */
     @GET(Endpoints.GET_CURRENT_WEATHER)
-    suspend fun getCurrentWeather(): Response<CurrentWeatherEntity>
+    suspend fun getCurrentWeather(): Response<CurrentWeatherResponse>
 
     /**
      * retrieves location weather status
      * @param locationId: [Int]
-     * @return location weather: [LocationEntity]
+     * @return location weather: [LocationResponse]
      */
     @GET(Endpoints.GET_LOCATION_WEATHER)
     suspend fun getLocationWeather(
         @Path("id") locationId: Int
-    ): Response<LocationEntity>
+    ): Response<LocationResponse>
 }

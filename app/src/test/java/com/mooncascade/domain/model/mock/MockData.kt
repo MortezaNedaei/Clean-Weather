2defe816b62f1e26aed3ac692fd90bfe5c65996e
Morteza Nedaei <nedaei1375@gmail.com>
2022-05-24 19:07:46 +0430

    Refactor(test-domain): change used data schema

--- app/src/test/java/com/mooncascade/domain/model/mock/MockData.kt
+++ app/src/test/java/com/mooncascade/domain/model/mock/MockData.kt
@@ -1,11 +1,10 @@
 package com.mooncascade.domain.model.mock
 
-const val currentWeatherMockData = "{\n" +
-        "  \"timestamp\": \"1652689045\"}"
+const val observationMockData = "[]"
 
 
 const val locationWeatherMockData = "{\n" +
         "  \"code\": \"26038\"}"
 
-const val nextDaysForecastsMockData = "{\n" +
-        "  \"date\": \"2022-05-17\"}"
+const val nextDaysForecastsMockData = "[{\n" +
+        "  \"date\": \"2022-05-17\"}]"

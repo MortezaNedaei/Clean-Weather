e33579d422e2d8a44be56e09e40f07b5b0549513
Morteza Nedaei <nedaei1375@gmail.com>
2022-05-24 19:22:53 +0430

    Feat(data): Add new data layer repository to get just remote data from location details

---
+++ app/src/main/java/com/mooncascade/data/respository/LocationDetailsDataRepository.kt
@@ -0,0 +1,37 @@
+package com.mooncascade.data.respository
+
+import android.util.Log
+import com.mooncascade.data.mapper.toDomain
+import com.mooncascade.data.network.service.WeatherApi
+import com.mooncascade.data.respository.datasource.base.BaseDataSource
+import com.mooncascade.di.qualifier.IoDispatcher
+import com.mooncascade.domain.model.location.Location
+import com.mooncascade.domain.respository.LocationDetailsRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class LocationDetailsDataRepository @Inject constructor(
+    private val weatherApi: WeatherApi,
+    @IoDispatcher private val coroutineDispatcher: CoroutineDispatcher
+) : BaseDataSource(), LocationDetailsRepository {
+
+    companion object {
+        private const val TAG = "WeatherDataRepository"
+    }
+
+
+    override suspend fun fetchLocationWeather(locationId: Int): Flow<Result<Location?>> = flow {
+        val response = getResult { weatherApi.getLocationWeather(locationId) }
+        response
+            .flowOn(coroutineDispatcher)
+            .catch { Log.e(TAG, "${it.message}") }
+            .collect {
+                emit(Result.success(it.getOrNull()?.toDomain()))
+            }
+    }
+
+}

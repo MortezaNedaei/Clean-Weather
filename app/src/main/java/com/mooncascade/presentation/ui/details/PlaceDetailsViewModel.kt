122cf281f454fbb327b382e659d8fc8e0722d3f9
Morteza Nedaei <nedaei1375@gmail.com>
2022-05-24 19:46:23 +0430

    Refactor(all): Add new Logger Tag retrieve possible length size for TAG

--- app/src/main/java/com/mooncascade/presentation/ui/details/PlaceDetailsViewModel.kt
+++ app/src/main/java/com/mooncascade/presentation/ui/details/PlaceDetailsViewModel.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import com.mooncascade.R
+import com.mooncascade.common.extensions.TAG
 import com.mooncascade.common.extensions.convertNumberToWords
 import com.mooncascade.di.qualifier.IoDispatcher
 import com.mooncascade.domain.interactor.GetLocationDetailsUseCase

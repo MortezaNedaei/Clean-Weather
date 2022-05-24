3501bd17e8ad20e6d2128e1cd13dfe42571217a4
Morteza Nedaei <nedaei1375@gmail.com>
2022-05-24 19:17:45 +0430

    Feat(common): add new context extension to check network

---
+++ app/src/main/java/com/mooncascade/common/extensions/Context.kt
@@ -0,0 +1,36 @@
+package com.mooncascade.common.extensions
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+fun Context.isNetworkConnected(): Boolean {
+    var result = false
+    val connectivityManager =
+        getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val networkCapabilities = connectivityManager.activeNetwork ?: return false
+        val activeNetwork =
+            connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+        result = when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_VPN) -> true
+            else -> false
+        }
+    } else {
+        connectivityManager.activeNetworkInfo?.run {
+            result = when (type) {
+                ConnectivityManager.TYPE_WIFI -> true
+                ConnectivityManager.TYPE_MOBILE -> true
+                ConnectivityManager.TYPE_ETHERNET -> true
+                ConnectivityManager.TYPE_VPN -> true
+                else -> false
+            }
+        }
+    }
+
+    return result
+}

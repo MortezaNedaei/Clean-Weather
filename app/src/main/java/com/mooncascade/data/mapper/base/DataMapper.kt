83c915bd92112775325c693bee18b7707202b7c1
Morteza Nedaei <nedaei1375@gmail.com>
2022-05-24 18:55:33 +0430

    Refactor(data): Update Base Mapper for domain-entity to use with inject method in other classes

--- app/src/main/java/com/mooncascade/data/mapper/base/DataMapper.kt
+++ app/src/main/java/com/mooncascade/data/mapper/base/DataMapper.kt
@@ -2,16 +2,26 @@ package com.mooncascade.data.mapper.base
 
 abstract class DataMapper<DomainModel, Entity> {
 
-    abstract fun transform(entity: Entity?): DomainModel?
+    abstract fun toDomain(entity: Entity?): DomainModel?
 
-    fun transform(collection: Collection<Entity>): List<DomainModel> {
+    fun toDomain(entities: List<Entity>): List<DomainModel> {
         val list = ArrayList<DomainModel>()
         var model: DomainModel?
-        for (entity in collection) {
-            model = transform(entity)
-            if (model != null) {
-                list.add(model)
-            }
+        for (entity in entities) {
+            model = toDomain(entity)
+            model?.let { list.add(it) }
+        }
+        return list
+    }
+
+    abstract fun toEntity(model: DomainModel?): Entity?
+
+    fun toEntity(models: List<DomainModel>): List<Entity> {
+        val list = ArrayList<Entity>()
+        var entity: Entity?
+        for (model in models) {
+            entity = toEntity(model)
+            entity?.let { list.add(it) }
         }
         return list
     }
